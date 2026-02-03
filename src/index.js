a0779f4d8506ae9bb962357bebeaa2a82ce76115
jagapathi20 <mallepulajagapathi20@gmail.com>
2026-02-03 21:10:35 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -3,3 +3,16 @@ import connectDB from "./db/index.js";
 
 loadEnvFile()
 connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("ERR:" ,error)
+        throw error
+    })
+    port = process.env.PORT || 8000
+    app.listen(port, () => {
+        console.log(`server is running at port: ${port}`)
+    })
+})
+.catch((err) => {
+    console.log("Mongo Db connection failed !!!", err)
+})
